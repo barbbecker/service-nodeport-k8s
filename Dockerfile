f19d3b74b26aa1299880045cd5d25706393c4567
Barbara Becker da Rosa <ilegra.rosa@agibank.com.br>
2021-05-16 16:34:07 -0300

    fix using variables

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.8.9-alpine
 
 RUN pip install flask
 
+ENV TESTE_BARBARA=barbara
+
 WORKDIR /app/
 
 COPY application.py .
--- application.py
+++ application.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+key = 'TESTE_BARBARA'
+value = os.getenv(key)
 
 @app.route('/app')
 def hello():
-    return 'Hello Word :)'
+    return('Hello Word %s' % value)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
